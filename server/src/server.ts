c72ddacbb18ca7e40b10c1e3e5ce6116dd7cf1ed
Mateus-Nunes <mateusnunes620@gmail.com>
2022-09-16 17:52:49 -0300

    server-update

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,16 +1,52 @@
 import express from 'express'
+import { PrismaClient } from '@prisma/client'
 
 const app = express()
+const prisma = new PrismaClient({
+  log: ['query']
+})
 
-// localhost:3333/ads
+// HTTP methods / API RESTful / HTTP Codes
 
-app.get('/ads', (request, response) => {
+/*
+Query: persistir estado, após a interrogação ?, nomeados
+Route: não nomeados, após a barra
+Body: enviar variás informações em uma unica requisição
+*/
+
+app.get('/games', async (request, response) => {
+  const games = await prisma.game.findMany({
+    include: {
+      _count: {
+        select: {
+          ads: true
+        }
+      }
+    }
+  })
+
+  return response.json(games)
+})
+
+app.post('/ads', (request, response) => {
+  return response.status(201).json([])
+})
+
+app.get('/games/:id/ads', (request, response) => {
+  // const gameId = request.params.id
+
   return response.json([
     { id: 1, name: 'Anúncio 1' },
     { id: 2, name: 'Anúncio 2' },
     { id: 3, name: 'Anúncio 3' },
     { id: 4, name: 'Anúncio 4' },
   ])
+})
+
+app.get('/ads/:id/discord', (request, response) => {
+  // const adId = request.params.id
+
+  return response.json([])
 })
 
 app.listen(3333)
